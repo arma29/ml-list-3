Fix issues related to normalization

src/classifiers/_kmeansbayes.py
@@ -1,9 +1,8 @@
-from sklearn.base import BaseEstimator, ClassifierMixin
-import numpy as np
-
-from sklearn.cluster import KMeans
-
 import math
+
+import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.cluster import KMeans
 
 
 class KMeansBayes(BaseEstimator, ClassifierMixin):
@@ -21,53 +20,43 @@ class KMeansBayes(BaseEstimator, ClassifierMixin):
         self.bayes_threshold = 0
         self.__exc_set = set([])
 
-    def fit(self, X, y, X_un=None):
+    def fit(self, X, y):
         self.__is_fitted = True
         self.__kmeans = KMeans(
             n_clusters=self.n_clusters, random_state=1).fit(X)
 
         self.__dist_dict = self.__initialize_dist_dict(X)
 
-        if(X_un is None):
-            return self
-
-        self.__initialize_stats_dict(X_un)
-        self.bayes_threshold = self.__compute_bayes_threshold(X_un)
+        self.__initialize_stats_dict(X)
+        self.bayes_threshold = self.__compute_bayes_threshold(X)
 
         return self
 
-    def __initialize_stats_dict(self, X_un):
+    def __initialize_stats_dict(self, X):
         # Pré computar medias e stds de cada att
-        for i in range(X_un.shape[1]):
-            column = X_un[:, i]
+        for i in range(X.shape[1]):
+            column = X[:, i]
             self.__mean_dict[str(i)] = np.mean(column)
             self.__std_dict[str(i)] = np.std(column)
 
-    def __compute_bayes_threshold(self, X_un):
-
-        arr = [self.__compute_bayes_individual(x) for x in X_un]
+    def __compute_bayes_threshold(self, X):
+        arr = [self.__compute_bayes_individual(x) for x in X]
         return min(arr)
 
     def __compute_bayes_individual(self, x):
         # Para cada atributo do exemplo
-        probs = 1
+        curr_prob = 1
         for i, att in enumerate(x):
             # Calcular a probabilidade do valor supondo uma gaussiana
             mean = self.__mean_dict[str(i)]
             std = self.__std_dict[str(i)]
 
-            if(std == 0):
-                custom_prob = 1
-            else:
-                # pdf = norm(loc=self.__mean_dict[str(i)],
-                #            scale=self.__std_dict[str(i)]).pdf(att)
+            if(std != 0):
                 fst_term = (1/(math.sqrt(2*math.pi*(std**2))))
                 snd_term = math.exp(-(((att - mean)**2)/(2*(std**2))))
-                custom_prob = fst_term * snd_term
+                curr_prob *= (fst_term * snd_term)
 
-            probs *= custom_prob
-
-        return probs
+        return curr_prob
 
     def __initialize_dist_dict(self, X):
         my_dict = {}
@@ -82,18 +71,19 @@ class KMeansBayes(BaseEstimator, ClassifierMixin):
 
         return my_dict
 
-    def predict(self, X, X_un=None):
+    def predict(self, X):
         if(not self.__is_fitted):
             raise Exception('Not fitted')
 
         predict_lst = []
 
         # Para cada dado de teste (Normalizado e Não-normalizado)
-        for x in zip(X, X_un):
-            dist_ratio = self.__compute_kmeans_ratio(x[0])
-            curr_prob = self.__compute_bayes_individual(x[1])
+        for x in X:
+            dist_ratio = self.__compute_kmeans_ratio(x)
+            curr_prob = self.__compute_bayes_individual(x)
 
-            if(dist_ratio > self.dist_threshold or curr_prob < self.bayes_threshold):  # Outlier
+            if(dist_ratio > self.dist_threshold
+               or curr_prob < self.bayes_threshold):  # Outlier
                 predict_lst.append(self.__pos_class)
             else:
                 predict_lst.append(self.__neg_class)
@@ -115,8 +105,8 @@ class KMeansBayes(BaseEstimator, ClassifierMixin):
 
         return dist_from_center / max_dist
 
-    def score(self, X, y, X_un=None):
-        return self.__accuracy_score(y, np.array(self.predict(X, X_un)))
+    def score(self, X, y):
+        return self.__accuracy_score(y, np.array(self.predict(X)))
 
     def __accuracy_score(self, y_true, y_pred):
         if(len(y_true) != len(y_pred)):
@@ -124,6 +114,8 @@ class KMeansBayes(BaseEstimator, ClassifierMixin):
         hits = [True for (a, b) in zip(y_true, y_pred) if a == b]
         return len(hits)/len(y_true)
 
-    def predict_proba(self, X, X_un=None):
-        predict_lst = self.predict(X, X_un)
-        return np.array([[1, 0] if x == self.__neg_class else [0, 1] for x in predict_lst])
+    def predict_proba(self, X):
+        predict_lst = self.predict(X)
+        return np.array([[1, 0]
+                         if x == self.__neg_class
+                         else [0, 1] for x in predict_lst])

src/processing.py
@@ -4,7 +4,7 @@ from os.path import isfile, join
 import numpy as np
 import pandas as pd
 from scipy.io import arff
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import MinMaxScaler
 
 from src.utils import get_project_data_dir
 
@@ -27,25 +27,28 @@ def process_data(dataset_path):
     data = arff.loadarff(dataset_path)
     df = pd.DataFrame(data[0])
 
-    # Criando conjunto de classes
-    if("datatrieve" in dataset_path):
-        df_neg = df.loc[df['Faulty6_1'] == b'0']
-        df_pos = df.loc[df['Faulty6_1'] == b'1']
-        target_names = np.array(['0', '1'])
-    else:  # cm1 is in path
-        df_neg = df.loc[df['defects'] == b'false']
-        df_pos = df.loc[df['defects'] == b'true']
-        target_names = np.array(['false', 'true'])
-
-    # Criando o conjunto  X_ e valores alvo (classes) y_
-    X_pos = df_pos.iloc[:, :-1].values
-    y_pos = df_pos.iloc[:, -1].values.astype('str')
-    X_neg = df_neg.iloc[:, :-1].values
-    y_neg = df_neg.iloc[:, -1].values.astype('str')
+    # Criando o conjunto de treinamento X_ e valores alvo (classes) y_
+    X = df.iloc[:, :-1].values
+    y = df.iloc[:, -1].values.astype('str')
 
     # Normalizando
-    X_pos_n = normalize(X=X_pos, axis=0, norm='max')
-    X_neg_n = normalize(X=X_neg, axis=0, norm='max')
+    min_max_scaler = MinMaxScaler()
+    X = min_max_scaler.fit_transform(X)
+
+    # Criando conjunto de classes
+    if("datatrieve" in dataset_path):
+        target_names = np.array(['0', '1'])
+    else:  # cm1 is in path
+        target_names = np.array(['false', 'true'])
+
+    for j, t in enumerate(target_names):
+        indexes = [i for i, yi in enumerate(y) if yi == t]
+        if(j == 0):
+            X_neg = X[indexes]
+            y_neg = y[indexes]
+        else:
+            X_pos = X[indexes]
+            y_pos = y[indexes]
 
     # Criando dicionário de retorno
     data_dict = {}
@@ -53,8 +56,6 @@ def process_data(dataset_path):
     data_dict['y_pos'] = y_pos
     data_dict['X_neg'] = X_neg
     data_dict['y_neg'] = y_neg
-    data_dict['X_pos_n'] = X_pos_n
-    data_dict['X_neg_n'] = X_neg_n
     data_dict['target_names'] = target_names
 
     return data_dict

src/training.py
@@ -18,16 +18,16 @@ def create_dict(data_dict):
         'y_pos': data_dict['y_pos'],
         'X_neg': data_dict['X_neg'],
         'y_neg': data_dict['y_neg'],
-        'X_pos_n': data_dict['X_pos_n'],
-        'X_neg_n': data_dict['X_neg_n'],
+        # 'X_pos_n': data_dict['X_pos_n'],
+        # 'X_neg_n': data_dict['X_neg_n'],
         'target_names': data_dict['target_names'],
         'dataset_name': data_dict['dataset_name'],
         'classifier': {
             '0.3': {
-                'bests': [12, 2.5]
+                'bests': [7, 1.2]
             },
             '0.4': {
-                'bests': [13, 2.1]
+                'bests': [13, 1.2]
             },
             '0.5': {
                 'bests': [8, 1.2]
@@ -36,11 +36,9 @@ def create_dict(data_dict):
     }
 
     if("cm1" in parameters_dict['dataset_name']):
-        parameters_dict['n_clusters_lst'] = [25, 26, 30]
-        parameters_dict['dist_th_lst'] = [1.1, 1.2]
-        parameters_dict['classifier']['0.3']['bests'] = [26, 1.1]
-        parameters_dict['classifier']['0.4']['bests'] = [25, 1.1]
-        parameters_dict['classifier']['0.5']['bests'] = [30, 1.2]
+        parameters_dict['classifier']['0.3']['bests'] = [30, 1.1]
+        parameters_dict['classifier']['0.4']['bests'] = [27, 2.4]
+        parameters_dict['classifier']['0.5']['bests'] = [23, 1.5]
 
     return parameters_dict
 
@@ -80,26 +78,15 @@ def train_model(data_dict):
         y_pos = parameters_dict['y_pos']
         X_neg = parameters_dict['X_neg']
         y_neg = parameters_dict['y_neg']
-        X_pos_n = parameters_dict['X_pos_n']
-        X_neg_n = parameters_dict['X_neg_n']
-
-        rd_state = 1
 
         X_neg_train, X_neg_test, y_neg_train, y_neg_test = \
             train_test_split(X_neg, y_neg, train_size=measure,
-                             random_state=rd_state)
+                             random_state=1)
 
         X_test = np.concatenate((X_pos, X_neg_test))
         y_test = np.concatenate((y_pos, y_neg_test))
 
-        X_neg_train_n, X_neg_test_n, y_neg_train, y_neg_test = \
-            train_test_split(X_neg_n, y_neg, train_size=measure,
-                             random_state=rd_state)
-
-        X_test_n = np.concatenate((X_pos_n, X_neg_test_n))
-
         parameters_dict['classifier'][str(measure)]['X_test'] = X_test
-        parameters_dict['classifier'][str(measure)]['X_test_n'] = X_test_n
         parameters_dict['classifier'][str(measure)]['y_test'] = y_test
 
         kmeans_bayes = KMeansBayes(
@@ -111,11 +98,9 @@ def train_model(data_dict):
         parameters_dict['classifier'][str(
             measure)]['X_neg_train'] = X_neg_train
         parameters_dict['classifier'][str(
-            measure)]['X_neg_train_n'] = X_neg_train_n
-        parameters_dict['classifier'][str(
             measure)]['y_neg_train'] = y_neg_train
         parameters_dict['classifier'][str(measure)]['obj'] = kmeans_bayes.fit(
-            X_neg_train_n, y_neg_train, X_neg_train)
+            X_neg_train, y_neg_train)
 
     parameters_dict['elapsed_time'] = time.time() - exp_time
 